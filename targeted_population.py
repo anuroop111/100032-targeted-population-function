16ac9c7c458cdac00bfc4e0a02919505bec10d72
taufique01 <taufiquehr@gmail.com>
2022-06-17 20:13:33 +0600

    add docstring for function input details

--- targeted_population.py
+++ targeted_population.py
@@ -7,10 +7,40 @@ from bernoulli_distribution import bernoulli_distribution
 
 def targeted_population(distribution_input, database_details, time_input, number_of_variable, stage_input_list):
     data, start_dowell_time, end_dowell_time = get_data_for_distribution(time_input, database_details)
-
     distribution_results = {}
     fields = database_details['fields']
     split = time_input['split']
+
+    """
+    don't change the above lines
+    ----------------------------------
+    example of the function parameter datatype
+    ----------------------------------
+    distribution_input={
+    'normal': 1,
+    'poisson':0,
+    'binomial':0,
+    'bernoulli':0
+    }
+    -----------------------------------
+    time_input = {
+    'column_name': 'Date',
+    'split': 'week',
+    'period': 'last_30_days',
+    'start_point': '2021/01/01',
+    'end_point': '2022/01/25',
+    }
+    -------------------------------------
+    'fields':['eventId', 'dowell_time']
+    -------------------------------------
+    number_of_variable is an integer value
+    --------------------------------------
+    stage_input_list = [
+    ]
+    --------------------------------------
+    distribution_input, database_details, time_input, number_of_variable, stage_input_list
+
+    """
 
     if distribution_input['normal'] == 1:
         distribution_results['normal'] = normal_distribution(data, stage_input_list, fields, number_of_variable)
