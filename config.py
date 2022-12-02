698d99497453c9a51911d2a505479ef8d8867e46
Md. Taufique Hasan <taufiquehr@gmail.com>
2022-12-02 14:47:43 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,1 @@
-HOST = "mongodb://user1:Test12345@cluster0-shard-00-00.n2ih9.mongodb.net:27017," \
-       "cluster0-shard-00-01.n2ih9.mongodb.net:27017," \
-       "cluster0-shard-00-02.n2ih9.mongodb.net:27017/Banglore?authSource=admin&replicaSet=atlas-heuz5b-shard-0" \
-       "&retryWrites=true&ssl=true&w=majority "
+
