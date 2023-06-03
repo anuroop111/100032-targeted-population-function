7460ade4bf4815a5840c953701d01b73b33a4c20
Md. Taufique Hasan <taufiquehr@gmail.com>
2023-06-03 11:03:06 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,22 +2,14 @@ import pymongo
 
 
 def fetch_collections(database):
-    my_client = pymongo.MongoClient(
-        "mongodb://user1:Test12345@cluster0-shard-00-00.n2ih9.mongodb.net:27017,"
-        "cluster0-shard-00-01.n2ih9.mongodb.net:27017,"
-        "cluster0-shard-00-02.n2ih9.mongodb.net:27017/Banglore?authSource=admin&replicaSet=atlas-heuz5b-shard-0"
-        "&retryWrites=true&ssl=true&w=majority")
+    my_client = pymongo.MongoClient("some keys")
 
     my_db = my_client[database]
     return my_db.list_collection_names()
 
 
 def fetch_databases():
-    my_client = pymongo.MongoClient(
-        "mongodb://user1:Test12345@cluster0-shard-00-00.n2ih9.mongodb.net:27017,"
-        "cluster0-shard-00-01.n2ih9.mongodb.net:27017,"
-        "cluster0-shard-00-02.n2ih9.mongodb.net:27017/Banglore?authSource=admin&replicaSet=atlas-heuz5b-shard-0"
-        "&retryWrites=true&ssl=true&w=majority")
+    my_client = pymongo.MongoClient("")
 
     dbs = my_client.list_database_names()
     return dbs
